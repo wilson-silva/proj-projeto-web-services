53a87e01d14db543d97c735ef57462cb006eafc3
wilson <wilson-gs@hotmail.com>
2023-12-05 17:06:58 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/educandoewb/course/config/TestConfig.java
+++ src/main/java/com/educandoewb/course/config/TestConfig.java
@@ -1,12 +1,14 @@
 package com.educandoewb.course.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoewb.course.entities.Order;
 import com.educandoewb.course.entities.User;
 import com.educandoewb.course.repositories.UserRepository;
 
@@ -20,8 +22,10 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+	
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456", List.of(new Order()));
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456", List.of(new Order()));
+		
 		
 		repository.saveAll(Arrays.asList(u1, u2));
 
--- src/main/java/com/educandoewb/course/entities/Order.java
+++ src/main/java/com/educandoewb/course/entities/Order.java
@@ -1,14 +1,14 @@
 package com.educandoewb.course.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,19 +18,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "tb_user")
-public class User implements Serializable {
+@Table(name = "tb_order")
+public class Order implements Serializable{
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
-	private String email;
-	private String phone;
-	private String password;
+	private Instant moment;
 	
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private User client;
 
 }
