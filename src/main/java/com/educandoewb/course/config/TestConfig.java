9fe8abdb41be013f3da421404ebb0b4a3d0cb8d0
wilson <wilson-gs@hotmail.com>
2024-01-03 14:20:40 -0300

    OrderStatus enum

--- src/main/java/com/educandoewb/course/config/TestConfig.java
+++ src/main/java/com/educandoewb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoewb.course.entities.Order;
 import com.educandoewb.course.entities.User;
+import com.educandoewb.course.entities.enums.OrderStatus;
 import com.educandoewb.course.repositories.OrderRepository;
 import com.educandoewb.course.repositories.UserRepository;
 
@@ -20,25 +21,22 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 
-	
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456", List.of(new Order()));
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456", List.of(new Order()));
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-		
-		
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
 		repository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-
 
 	}
 
--- src/main/java/com/educandoewb/course/entities/Order.java
+++ src/main/java/com/educandoewb/course/entities/Order.java
@@ -3,9 +3,8 @@ package com.educandoewb.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.educandoewb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,22 +18,68 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private User client;
+
+	public Order() {
+
+	}
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		setOrderStatus(orderStatus);
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOfCodeToValue(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
 
 }
